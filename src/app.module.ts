bcee334ce9a11923b572bcf94cb516ad4900380c
AkwinderDhiman <akwinder128@gmail.com>
2024-04-11 18:40:21 +0530

    Link with neon prisma db, create .env file,  connect with database, and ----add, update, delete and get employees

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { DatabaseModule } from './database/database.module';
+import { EmployeesModule } from './employees/employees.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, DatabaseModule, EmployeesModule],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
